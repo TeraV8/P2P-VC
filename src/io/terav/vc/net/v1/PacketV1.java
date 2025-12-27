556f3d5b301f67c6316f38c0eddb364b6514b35c
TeraV8 <89698857+TeraV8@users.noreply.github.com>
2025-12-27 11:54:10 -0500

    Fixed EchoPacket transform

--- src/io/terav/vc/net/v1/PacketV1.java
+++ src/io/terav/vc/net/v1/PacketV1.java
@@ -13,7 +13,7 @@ public abstract class PacketV1 extends Packet {
     public static Packet parse(int packet_id, short version, byte flags, byte recipient, ByteBuffer buffer) {
         if (((version >> 8) & 0xFF) != 1) throw new IllegalArgumentException("Invalid packet version");
         return switch (flags & 0x3) {
-            case 0 -> new EchoPacket(packet_id); // screw consistency because this is all that is necessary
+            case 0 -> EchoPacket.parse(packet_id, buffer);
             case 1 -> DataPacket.parse(packet_id, recipient, buffer);
             case 2, 3 -> ProtoPacket.parse(packet_id, flags, recipient, buffer);
             default -> null;
