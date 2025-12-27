08d600262899b0b6a52020b7f11884d5074000ce
TeraV8 <89698857+TeraV8@users.noreply.github.com>
2025-12-27 11:51:44 -0500

    Added v1 echo logic

--- src/io/terav/vc/net/v1/ProtocolV1.java
+++ src/io/terav/vc/net/v1/ProtocolV1.java
@@ -2,13 +2,34 @@ package io.terav.vc.net.v1;
 
 import io.terav.vc.NetworkManager;
 import io.terav.vc.net.PeerInfo;
+import java.util.HashMap;
+import java.util.Random;
 
 public final class ProtocolV1 {
+    // key: echo packet data    value: timestamp of outbound packet if positive, if negative it is negative rebound time
+    private static final HashMap<Long, Long> pendingEchoes = new HashMap<>();
     // TODO stuf
     private ProtocolV1() {}
     
     public static void activateProtocolProcessor() {
-        
+        NetworkManager.addPacketHook((packet, peer) -> {
+            if (packet instanceof EchoPacket echo) {
+                if (pendingEchoes.containsKey(echo.data) && pendingEchoes.get(echo.data) > 0)
+                    pendingEchoes.put(echo.data, pendingEchoes.get(echo.data) - System.currentTimeMillis());
+                else
+                    NetworkManager.sendPacket(new EchoPacket(peer.nextPacketId(), echo.data), peer.remote);
+            }
+            return false;
+        });
+    }
+    public static void sendEcho(PeerInfo peer) {
+        Random random = new Random();
+        long data;
+        do {
+            data = random.nextLong();
+        } while (pendingEchoes.containsKey(data));
+        pendingEchoes.put(data, System.currentTimeMillis());
+        NetworkManager.sendPacket(new EchoPacket(peer.nextPacketId(), data), peer.remote);
     }
     public static void connectVC(PeerInfo peer, String message) {
         
