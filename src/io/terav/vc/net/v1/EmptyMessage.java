8689aae4f7ba3e84712d534260a21000911d6efa
TeraV8 <89698857+TeraV8@users.noreply.github.com>
2025-12-27 11:04:38 -0500

    v1/EmptyMessage is sealed now

--- src/io/terav/vc/net/v1/EmptyMessage.java
+++ src/io/terav/vc/net/v1/EmptyMessage.java
@@ -6,7 +6,7 @@ import java.nio.ByteBuffer;
  * Placeholder class for all message types that have no data.
  * @author terav
  */
-public class EmptyMessage extends Message {
+public final class EmptyMessage extends Message {
     public static final byte PROTO_REQUEST_IDENTIFY = 0x75;
     public static final byte PROTO_DOWNGRADE = 0x7E;
 
