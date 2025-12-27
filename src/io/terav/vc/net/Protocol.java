d1988f9a33b2fd11f3703024487fd82e8f6be40d
TeraV8 <89698857+TeraV8@users.noreply.github.com>
2025-12-27 14:22:07 -0500

    Added universal echo and version intermix logic

--- src/io/terav/vc/net/Protocol.java
+++ src/io/terav/vc/net/Protocol.java
@@ -10,6 +10,16 @@ public class Protocol {
     
     private Protocol() {}
     
+    private static byte getVersion(PeerInfo peer) {
+        if (peer.protover_compat != -1)
+            return (byte) (peer.protover_compat >> 8);
+        if (peer.protover_hi == -1)
+            return HIGHEST_PROTOVER;
+        if (((peer.protover_hi >> 8) & 0xFF) >= HIGHEST_PROTOVER)
+            return HIGHEST_PROTOVER;
+        peer.protover_compat = (short) ((peer.protover_hi & 0xFF00) | 0xFF);
+        return (byte) (peer.protover_hi >> 8);
+    }
     public static synchronized void activateProtocolProcessor(byte version) {
         if (version == -1) version = HIGHEST_PROTOVER;
         if ((version & 0xFF) > HIGHEST_PROTOVER) throw new UnsupportedOperationException("Protocol version " + (version & 0xFF) + " not supported");
@@ -19,5 +29,11 @@ public class Protocol {
             case 1 -> ProtocolV1.activateProtocolProcessor();
         }
         activatedProtocolProcessors[version] = true;
+    }
+    public static void sendEcho(PeerInfo peer) {
+        switch (getVersion(peer)) {
+            case 0 -> ProtocolV0.sendEcho(peer);
+            case 1 -> ProtocolV1.sendEcho(peer);
+        }
     }
 }
